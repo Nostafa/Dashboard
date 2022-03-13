33981146a003a1180c9a8a233a562c41a64a7905
Mustafa <mostafamok.mm@gmail.com>
2022-03-13 04:59:16 +0200

    creating Navbar and Sidebar

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import Sidebar from '../../components/sidebar/Sidebar';
+import Navbar from '../../components/navbar/Navbar';
 import './Home.scss';
 const Home = () => {
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      <Sidebar />
+      <div className="homeContainer">
+        <Navbar />
+        homeContainer
+      </div>
+    </div>
+  );
 };
 
 export default Home;
