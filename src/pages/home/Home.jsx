6df8d945d11307f5791a4025f22e228f65beaa51
Mustafa <mostafamok.mm@gmail.com>
2022-03-15 15:58:54 +0200

    finshed my DataTabel , charts and cards components and list page and home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,31 @@
+import './Home.scss';
 import Sidebar from '../../components/sidebar/Sidebar';
 import Navbar from '../../components/navbar/Navbar';
-import './Home.scss';
+import Widget from '../../components/widget/Widget';
+import Featured from '../../components/featured/Featured';
+import Chart from '../../components/chart/Chart';
+import List from '../../components/list/List';
+
 const Home = () => {
   return (
     <div className="home">
       <Sidebar />
       <div className="homeContainer">
         <Navbar />
-        homeContainer
+        <div className="widgets">
+          <Widget type="user" />
+          <Widget type="orders" />
+          <Widget type="earning" />
+          <Widget type="balance" />
+        </div>
+        <div className="charts">
+          <Featured />
+          <Chart />
+        </div>
+        <div className="listContainer">
+          <div className="listTitle">Last Transections</div>
+          <List />
+        </div>
       </div>
     </div>
   );
